ef9a7eaea012ece3d35f60924f71a2a1531dc285
LDEERL1 <148969844+LDEERL1@users.noreply.github.com>
2026-02-12 15:23:51 +0700

    render + program

--- DeviceStatsT/DeviceStatsT/Program.cs
+++ DeviceStatsT/DeviceStatsT/Program.cs
@@ -28,14 +28,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
 app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
