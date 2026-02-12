3c64a49861bd0c70498076f8cd80a45b3146de5f
LDEERL1 <148969844+LDEERL1@users.noreply.github.com>
2026-02-12 17:41:08 +0700

    fix: production environment config for SPA v2

--- DeviceStatsT.SPA/spa/DeviceStatsTSpa/src/app/devices-api.ts
+++ DeviceStatsT.SPA/spa/DeviceStatsTSpa/src/app/devices-api.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs'
-export interface Device{
-    id: string;
-    name: string;
-    version: string;
-  }
-  export interface DeviceStat{
-    startTime: string;
-    endTime: string;
-  }
-@Injectable({
-  providedIn: 'root',
-})
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
+export interface Device {
+  id: string;
+  name: string;
+  version: string;
+}
+
+export interface DeviceStat {
+  startTime: string;
+  endTime: string;
+}
+
+@Injectable({ providedIn: 'root' })
 export class DevicesApi {
-  constructor(private http: HttpClient) {}
-  
-  getDevices(): Observable<Device[]>{
-    return this.http.get<Device[]>('https://localhost:7286/api/devices');
-  }
- getDeviceStats(deviceId: string): Observable<DeviceStat[]>{
-   return this.http.get<DeviceStat[]>(`https://localhost:7286/api/devices/${deviceId}/stats`);
+  private readonly baseUrl = environment.apiBaseUrl; 
 
- }
+  constructor(private http: HttpClient) {}
+
+  getDevices(): Observable<Device[]> {
+    return this.http.get<Device[]>(`${this.baseUrl}/api/devices`);
+  }
+
+  getDeviceStats(deviceId: string): Observable<DeviceStat[]> {
+    return this.http.get<DeviceStat[]>(`${this.baseUrl}/api/devices/${deviceId}/stats`);
+  }
 }
